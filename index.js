f0bedbe717397b0b50cb75f427c2f46a919fef4e
Shoxjaxon-com <alijonovshohjahon03@gmail.com>
2024-10-12 05:45:54 -0700

    vazifa

---
+++ index.js
@@ -0,0 +1,215 @@
+//  === Oson masalalar === //
+
+// 1-masala
+// function kvadrat (arg){
+//  return arg*2
+// }
+// console.log(kvadrat(2));
+
+// 2-masala
+
+// function yigindi(arg1,arg2){
+//     return arg1 + arg2
+// }
+// console.log(yigindi(2,5));
+
+// 3-masala
+// function ism(arg){
+//     return `Salom ${arg}`
+// }
+// console.log(ism('Shahjahon'));
+
+// 4-masala
+
+// let arr = [1,2]
+// function yigindi(arg){
+//    let max = Math.max(...arr)
+//    return max
+// }
+// console.log(yigindi(arr));
+
+// 5-masala
+
+// let arr=[1,2,3]
+// function yigindi(arg){
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         sum = sum+arr[i]
+//     }
+//     return sum
+// }
+// console.log(yigindi(arr));
+
+// 6-masala
+// function musbat(arg){
+//     if(arg > 0){
+//         return 'Musbat son'
+//     }else{
+//         return 'Manfiy son'
+//     }
+// }
+// console.log(musbat(2));
+
+// 7-masala
+// function uzulig(arg){
+//     return arg.length;
+// }
+// console.log(uzulig('Salom'));
+
+// 8-masala
+// function soz(arg1,arg2){
+// return arg1+arg2
+// }
+// console.log(soz('Salom' , 'Dunyo'));
+
+// 9-masala
+// let arr = [1,2]
+// function yigindi(arg){
+//    let min = Math.min(...arr)
+//    return min
+// }
+// console.log(yigindi(arr));
+
+// 10-masala
+// function teskari(arg){
+//     let res = arg.split("").reverse().join('')
+//     return res;
+// }
+// console.log(teskari('Salom'));
+
+// === Massiv va funksiyalar === //
+
+// 1-masala
+
+// let arr =[1,2,3,4,5,6,7]
+// function massiv(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         return arr;
+//     }
+// }
+// console.log(massiv(arr));
+
+// 2-masala
+
+// let arr = [1,2]
+// function yigindi(arg){
+//    let max = Math.max(...arr)
+//    return max
+// }
+// console.log(yigindi(arr));
+
+// 3-masala
+// let arr =[1,2,3,4,5,6,7]
+// function massiv(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         return arr.length;
+// }
+// }
+// console.log(massiv(arr));
+
+// 4-masala
+// let arr = [1,2,3,4,5,6,7,8,9]
+// function yigindi(arr){
+//     let sum =0;
+//     for(let i = 0; i < arr.length;i++){
+//         sum+=arr[i]
+//     }
+//     return sum
+// }
+// console.log(yigindi(arr));
+
+// 5-masala
+// let arr =[1,2,3,4,2,5,6,7,8,9]
+// let elemt =2;
+// function sum(arg1,arg2){
+//     let count =0;
+//     for(let i = 0; i <arr.length; i++){
+//         if(arr[i] === elemt){
+//             count++
+//         }
+//     }
+//     return count
+// }
+// console.log(sum(arr,elemt));
+
+// 6-masala
+
+// let arr =[1,2,3,4,5,6,7]
+// let res =[]
+// function massiv(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] % 2 == 1){
+//             res.push(arr[i])
+//         }
+//     }
+//     return res
+// }
+// console.log(massiv(arr));
+
+// 7-masala
+
+// let arr =[1,2,3,4,5,6,7]
+// function massiv(arr){
+//     return res
+
+// }
+// console.log(massiv(arr));
+
+// 8-masala
+// let arr =[-2,2,-3,4,-6,-7,-8,]
+// let res =[]
+// function yigindi(arr){
+//     for(let i = 0; i< arr.length; i++){
+//         if(arr[i] < 0){
+//             res.push(arr[i])
+//         }
+//     }
+//     return res
+// }
+// console.log(yigindi(arr));
+
+// 9-masala
+// let arr =[1,2,3,4,5,6,7]
+// function massiv(arr){
+//   arr.pop();
+//   return arr
+// }
+// console.log(massiv(arr));
+
+// 10-masala
+
+// let arr =[1,2,3,4,5,6,7]
+// function massiv(arr){
+//   arr.push(11);
+//   return arr
+// }
+// console.log(massiv(arr));
+
+// 11-masala
+// let arr =[1,2,3]
+// let arr2 =[4,5,6]
+// function kvadrat (arr ,arr2){
+//     return arr.concat(arr2)
+// }
+//    console.log(kvadrat(arr,arr2));
+
+// 12-masala
+// let arr =[1,2,3,4,5,6,7]
+// function massiv(arr){
+//     return arr.map(num => num*num)
+// }
+// console.log(massiv(arr));
+
+// 13-masala
+// let arr =[1,2,3,4,5,6,7]
+// let res =[]
+// function massiv(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] % 2 != 0){
+//             res.push(arr[i])
+//         }
+//     }
+//     return res
+// }
+// console.log(massiv(arr));
+
